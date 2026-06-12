a23b77c10cfa5ed7675b165640cf910a68c9dfe5
loganyeh <yehlogan@gmail.com>
2026-06-12 15:41:48 -0400

    Overview page static ui done

--- frontend/src/pages/Home/Activity.tsx
+++ frontend/src/pages/Home/Activity.tsx
@@ -1,38 +1,48 @@
 
 
-function Activity(){
+type ActivityProps = {
+    page: "Home" | "Overview",
+};
+
+
+function Activity({ page }: ActivityProps ){
 
 
     return(
         <>
-            <section className="flex flex-col gap-4 w-full">
+            <section className="flex flex-col gap-5 w-full">
                 <div className="flex justify-between items-center">
-                    <h1 className="px-4 py-2 text-sm">Activity</h1>
+                    <h1 className={`${page === "Overview" ? "font-medium" : ""} px-4 text-sm`}>Activity</h1>
 
-                    <div className="flex gap-2 items-center">
+                    {page === "Home" && <div className="flex gap-2 items-center">
                         <i className='bx bxs-filter-alt text-base'></i>
 
                         <div className="flex text-xs font-light bg-white rounded">
                             <p className="px-3 py-1.5 font-medium">Following</p>
                             <p className="px-3 py-1.5">Global</p>
                         </div>
-                    </div>
+                    </div>}
+
+                    {page === "Overview" && <div className="flex items-center">
+                        <p className="text-xs">Filter</p>
+                        <i className='bx bx-chevron-down text-xl' ></i>
+                    </div>}
                 </div>
 
                 <input type="text" className="px-4 py-2 text-sm bg-white rounded" placeholder="Write a status..." />
 
-                <div className="flex flex-col gap-5">
+                <div className={`${page === "Overview" ? "2xl:grid 2xl:grid-cols-2" : ""} flex flex-col gap-5`}>
                 {/* <div className="border border-red-600 flex flex-col gap-5"> */}
 
-                    {Array.from({ length: 2 }).map((_, index) => {
+                    {Array.from({ length: 6 }).map((_, index) => {
                         return <div key={index} className="flex rounded shadow">
-                            <div className="w-20 aspect-[3/4] bg-blue-300 rounded-l"></div>
+                            <div className={`${page === "Overview" ? "w-14" : "w-24 md:w-[88px] 2xl:w-20"}  aspect-[3/4] bg-blue-300 rounded-l`}></div>
 
-                            <div className="flex justify-between gap-2 p-3.5 w-full bg-white rounded">
-                                <div className="flex flex-col gap-2 justify-between text-xs">
-                                    <p className="text-blue-300">jujune</p>
-                                    <p>Plans to watch Frieren: Beyond Journey's End Season 2</p>
-                                    <div className="w-10 aspect-square bg-blue-300 rounded"></div>
+                            <div className="flex justify-between gap-6 p-3.5 w-full bg-white rounded">
+                                <div className="flex flex-col gap-2 2xl:gap-1 justify-between text-xs">
+                                    <p className={`${page === "Overview" ? "hidden" : ""} text-blue-300`}>jujune</p>
+                                    <p className={`${page === "Overview" ? "flex items-center h-full text-sm" : "text-xs"}`}>Plans to watch Frieren: Beyond Journey's End Season 2</p>
+                                    <div className={`${page === "Overview" ? "hidden" : ""} w-10 aspect-square bg-blue-300 rounded`}></div>
                                 </div>
 
                                 <div className="flex flex-col justify-between items-end text-[11px] shrink-0">
--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -19,7 +19,7 @@ function Home(){
             <div className="flex justify-center px-5 pt-10 py-14 bg-gray-200">
                 <div className="flex flex-col xl:flex-row gap-5 xl:gap-10 max-w-5xl 2xl:max-w-7xl w-full">
                     <div className="hidden xl:flex xl:flex-1">
-                        <Activity />
+                        <Activity page="Home" />
                     </div>
 
                     <div className="flex flex-col gap-5 xl:max-w-md w-full">
@@ -33,7 +33,7 @@ function Home(){
                     </div>
 
                     <div className="xl:hidden">
-                        <Activity />
+                        <Activity page="Home" />
                     </div>
                 </div>
             </div>
