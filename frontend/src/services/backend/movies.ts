b77d9f4e8e47e598b187859748634401d648b7ff
loganyeh <yehlogan@gmail.com>
2026-07-11 13:14:48 -0400

    added env var

--- frontend/src/services/backend/movies.ts
+++ frontend/src/services/backend/movies.ts
@@ -1,28 +1,29 @@
 import type { MovieListsType } from "../tmdb/movieLists";
+const API_URL = import.meta.env.VITE_API_URL;
 
 export async function fetchAllMovies(){
-    const res = await fetch(`https://moviecritic-mi3f.onrender.com/movies/`);
+    const res = await fetch(`${API_URL}/movies/`);
     const data: MovieListsType[] = await res.json();
 
     return data;
 };
 
 export async function fetchFavMovies(){
-    const res = await fetch(`https://moviecritic-mi3f.onrender.com/movies/favorites/`);
+    const res = await fetch(`${API_URL}/movies/favorites/`);
     const data: MovieListsType[] = await res.json();
 
     return data;
 };
 
 export async function fetchWatchingMovies(){
-    const res = await fetch(`https://moviecritic-mi3f.onrender.com/movies/watching`);
+    const res = await fetch(`${API_URL}/movies/watching`);
     const data: MovieListsType[] = await res.json();
 
     return data;
 };
 
 export async function fetchCompletedMovies(){
-    const res = await fetch(`https://moviecritic-mi3f.onrender.com/movies/completed`);
+    const res = await fetch(`${API_URL}/movies/completed`);
     const data = await res.json();
 
     return data;
