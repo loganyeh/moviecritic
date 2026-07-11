04dafa9eb9bc7da12f6b00e111c296c5af88f937
loganyeh <yehlogan@gmail.com>
2026-07-11 13:06:19 -0400

    changed to render url

--- frontend/src/services/backend/movies.ts
+++ frontend/src/services/backend/movies.ts
@@ -1,28 +1,28 @@
 import type { MovieListsType } from "../tmdb/movieLists";
 
 export async function fetchAllMovies(){
-    const res = await fetch(`http://localhost:3000/movies/`);
+    const res = await fetch(`https://moviecritic-mi3f.onrender.com/movies/`);
     const data: MovieListsType[] = await res.json();
 
     return data;
 };
 
 export async function fetchFavMovies(){
-    const res = await fetch(`http://localhost:3000/movies/favorites/`);
+    const res = await fetch(`https://moviecritic-mi3f.onrender.com/movies/favorites/`);
     const data: MovieListsType[] = await res.json();
 
     return data;
 };
 
 export async function fetchWatchingMovies(){
-    const res = await fetch(`http://localhost:3000/movies/watching`);
+    const res = await fetch(`https://moviecritic-mi3f.onrender.com/movies/watching`);
     const data: MovieListsType[] = await res.json();
 
     return data;
 };
 
 export async function fetchCompletedMovies(){
-    const res = await fetch(`http://localhost:3000/movies/completed`);
+    const res = await fetch(`https://moviecritic-mi3f.onrender.com/movies/completed`);
     const data = await res.json();
 
     return data;
